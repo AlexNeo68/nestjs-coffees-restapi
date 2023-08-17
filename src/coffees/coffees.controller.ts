a552418f0a54b2478d863ce6c3d2b6c5d5b23821
Alexander <alexander@iMac-Alexander.local>
2023-08-17 21:43:13 +0300

    tage

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -3,14 +3,11 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Logger,
   Param,
   Patch,
   Post,
   Query,
-  SetMetadata,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -19,13 +16,16 @@ import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { ParseIntPipe } from 'src/common/pipes/parse-int/parse-int.pipe';
 import { Protocol } from 'src/common/decorators/protocol/protocol.decorator';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   private logger = new Logger(CoffeesController.name);
 
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ type: 'Forbidden' })
   @Public()
   @Get()
   findAll(
--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
-import { Flavor } from '../entities/flavor.entity';
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'Name of a coffee' })
   @IsString()
   readonly name: string;
+  @ApiProperty({ description: 'Description of a coffee' })
   @IsString()
   readonly description: string;
+  @ApiProperty()
   @IsString()
   readonly brand: string;
+  @ApiProperty({ example: [] })
   @IsString({ each: true })
   readonly flavors: string[];
 }
